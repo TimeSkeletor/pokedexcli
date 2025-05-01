2accd3fc15d35556f5d3d9a3461c90a23d9d40d9
TimeSkeletor <timeskeletor@gmail.com>
2025-05-02 00:13:03 +0200

    Add explore command

--- commands.go
+++ commands.go
@@ -6,33 +6,12 @@ import (
 	"errors"
 )
 
-func getCommands() map[string]cliCommand {
-    return map[string]cliCommand{
-		"help": {
-			name:        "help",
-			description: "Get instructions to use the Pokedex",
-			callback:    commandHelp,
-		},
-		"map": {
-			name:        "map",
-			description: "Get the next page of locations",
-			callback:    commandMapf,
-		},
-		"mapb": {
-			name:        "mapb",
-			description: "Get the previous page of locations",
-			callback:    commandMapb,
-		},
-        "exit": {
-            name:        "exit",
-            description: "Exit the Pokedex",
-            callback:    commandExit,
-        },
-    }
-}
 
+const (
+	replMsg = "Pokedex > "
+)
 
-func commandHelp(cfg *config) error {
+func commandHelp(cfg *config, args ...string) error {
 	fmt.Println("Usage:")
 
 	for _, cmd := range getCommands() {
@@ -43,7 +22,25 @@ func commandHelp(cfg *config) error {
 }
 
 
-func commandMapf(cfg *config) error {
+func commandExplore(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a location name")
+	}
+
+	name := args[0]
+	location, err := cfg.pokeapiClient.GetLocation(name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Exploring %s...\n", location.Name)
+	fmt.Println("Found Pokemon: ")
+	for _, enc := range location.PokemonEncounters {
+		fmt.Printf(" - %s\n", enc.Pokemon.Name)
+	}
+	return nil
+}
+
+func commandMapf(cfg *config, args ...string) error {
 	locationsResp, err := cfg.pokeapiClient.GenericGetList(cfg.nextPageURL, "location-area")
 	if err != nil {
 		return err
@@ -58,7 +55,7 @@ func commandMapf(cfg *config) error {
 	return nil
 }
 
-func commandMapb(cfg *config) error {
+func commandMapb(cfg *config, args ...string) error {
 	if cfg.prevPageURL == nil {
 		return errors.New("you're on the first page")
 	}
@@ -77,7 +74,7 @@ func commandMapb(cfg *config) error {
 	return nil
 }
 
-func commandExit(cfg *config) error {
+func commandExit(cfg *config, args ...string) error {
 	fmt.Println(replMsg, "Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
--- repl.go
+++ repl.go
@@ -9,26 +9,16 @@ import (
 	"github.com/timeskeletor/pokedexcli/internal/pokeapi"
 )
 
-const (
-	replMsg = "Pokedex > "
-)
 type config struct {
 	pokeapiClient    pokeapi.Client
 	nextPageURL *string
 	prevPageURL *string
 }
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func(*config) error
-}
-
-
 func startRepl(cfg *config) {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Print(replMsg)
+		fmt.Print("Pokedex > ")
 		reader.Scan()
 
 		words := cleanInput(reader.Text())
@@ -37,16 +27,20 @@ func startRepl(cfg *config) {
 		}
 
 		commandName := words[0]
+		args := []string{}
+		if len(words) > 1 {
+			args = words[1:]
+		}
 
 		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback(cfg)
+			err := command.callback(cfg, args...)
 			if err != nil {
 				fmt.Println(err)
 			}
 			continue
 		} else {
-			fmt.Println(replMsg, "Unknown command")
+			fmt.Println("Unknown command")
 			continue
 		}
 	}
@@ -56,4 +50,40 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(*config, ...string) error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"explore": {
+			name:        "explore <location_name>",
+			description: "Explore a location",
+			callback:    commandExplore,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
